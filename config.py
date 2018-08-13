2b211dd7270dee98cb1335b5f5afb4950c463b1b
wenbo_zhu <1032988752@qq.com>
2018-08-13 13:33:22 +0800

    修改数据库地址

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class DevelopmentConfig(Config):
     开发环境
     """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:111111@127.0.0.1:3306/py_test2?charset=utf8'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:111111@192.168.9.184:3306/py_test2?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class ProductConfig(Config):
@@ -23,8 +23,8 @@ class ProductConfig(Config):
     正式环境
     """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:111111@127.0.0.1:3306/py_test?charset=utf8'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:111111@192.168.9.184:3306/py_test?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 config = {
